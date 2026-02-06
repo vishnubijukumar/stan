6b7cb7edb5a21e764bfb44f7eaf3d79611f6d463
vishnubijukumar <vishnubijukumar@ibm.com>
2026-02-06 15:51:16 +0000

    Final commit on stage_2
    Signed-off-by: vishnubijukumar <vishnubijukumar@ibm.com>

--- stan.c
+++ stan.c
@@ -3,30 +3,41 @@
 #include<termios.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<errno.h>
 
 struct termios orig_termios;
 
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios) == -1) die("tcsetattr");
 }
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO,&orig_termios);
+    if(tcgetattr(STDIN_FILENO,&orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
     struct termios raw=orig_termios;
     tcgetattr(STDIN_FILENO,&raw);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK);
     raw.c_oflag &= ~(OPOST);
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+    raw.c_cflag &= ~(CS8);
+    raw.c_cc[VMIN]=0;
+    raw.c_cc[VTIME]=1;
+
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw) == -1) die("tcsetattr");
 }
 int main()
 {
     enableRawMode();
-    char c;
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q')
+    while(1)
     {
+        char c = '\0';
+        if(read(STDIN_FILENO , &c , 1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c))
         {
             printf("%c\r\n",c);
@@ -35,6 +46,7 @@ int main()
         {
             printf("%d ('%c')\r\n",c,c);
         }
+        if(c == 'q') break;
     }
     return 0;
 }
