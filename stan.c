ef92a50b9a9d282fae776939428f417af425ae65
vishnubijukumar <vishnubijukumar@ibm.com>
2026-02-11 16:57:24 +0000

    Step-21
    Signed-off-by: vishnubijukumar <vishnubijukumar@ibm.com>

--- stan.c
+++ stan.c
@@ -5,6 +5,8 @@
 #include<ctype.h>
 #include<errno.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s)
@@ -31,22 +33,35 @@ void enableRawMode()
 
     if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw) == -1) die("tcsetattr");
 }
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while((nread=read(STDIN_FILENO , &c , 1)) != 1)
+    {
+        if(nread == -1 && errno !=EAGAIN ) die("read");
+    }
+    return c;
+}
+
+void editorProcessKeypress()
+{
+    char c= editorReadKey();
+    switch(c)
+    {
+        case CTRL_KEY('q'): exit(0);
+        break;
+
+    }
+}
+
 int main()
 {
     enableRawMode();
     while(1)
     {
-        char c = '\0';
-        if(read(STDIN_FILENO , &c , 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c))
-        {
-            printf("%c\r\n",c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n",c,c);
-        }
-        if(c == 'q') break;
+        editorProcessKeypress();
     }
     return 0;
 }
